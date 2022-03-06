cdcde30c3f9375f20f41b80655cfc5d01297120e
DESKTOP-CQBPQTE\user <zoo@sookmyung.ac.kr>
2022-03-06 17:45:00 +0900

    no message

---
+++ KotlinPractice/app/src/main/java/com/example/kotlinpractice/Day01.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinpractice
+
+fun main(){
+    val a: Int = 66
+    val b: Double = a.toDouble()
+    val c: Byte = a.toByte()
+    val d: Float = a.toFloat()
+    val e: Long = a.toLong()
+    val f: Char = a.toChar()
+
+    println("a: $a")
+    println("b: $b")
+    println("c: $c")
+    println("d: $d")
+    println("e: $e")
+    println("f: $f")
+
+}
